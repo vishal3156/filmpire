11820bdf3664e5d2099c5468b51ce13d0657d56e
Vishal Suman <119152809+vishal3156@users.noreply.github.com>
2023-04-27 22:28:17 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import React from "react";
+import React, {useState} from 'react';
 
-class Counter extends React.Component {
-  constructor(props) {
-    super(props);
+export default function ColorPicker() {
+ const [color, setColor] = useState();
 
-    this.state = {
-      count: 0,
-    };
+ const divStyle = {backgroundColor: color};
 
-    this.increment = () => this.setState({ count: this.state.count + 1 });
-    this.decrement = () => this.setState({ count: this.state.count - 1 });
-  }
-
-  componentDidMount() {
-    console.log("Component has mounted."); //
-  }
-
-  componentDidUpdate() {
-    console.log("Component updated. Count is now " + this.state.count + ".");
-  }
-
-  render() {
-    console.log("Rendering...");
-    return (
-      <React.Fragment>
-        <h1>Count: {this.state.count}</h1>
-        <button onClick={this.increment}>+1</button>
-        <button onClick={this.decrement}>-1</button>
-      </React.Fragment>
-    );
-  }
+  return (
+    <div style={divStyle}>
+      {/* <p>The color is {color}</p> */}
+      <button onClick={() => setColor('Aquamarine')}>
+        Aquamarine
+      </button>
+      <button onClick={() => setColor('BlueViolet')}>
+        BlueViolet
+      </button>
+      <button onClick={() => setColor('Chartreuse')}>
+        Chartreuse
+      </button>
+      <button onClick={() => setColor('CornflowerBlue')}>
+        CornflowerBlue
+      </button>
+    </div>
+  );
 }
-
-ReactDOM.render(<Counter />, document.getElementById("root"));
